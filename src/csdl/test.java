4e2ea23964b4d466a062cb5362223e6896ca21ef
BachDuyHoang <bachduyhoang2k@gmail.com>
2020-05-26 13:05:06 +0700

    add hh

--- src/csdl/test.java
+++ src/csdl/test.java
@@ -12,9 +12,7 @@ package csdl;
 public class test {
     public static void main(String[] args) {
         System.out.println("Add Main");
-        System.out.println("Hoang dfsfdsfsdf ");
-        for (int i=0 ; i<10; i++){
-            System.out.println("i");
+            System.out.println("Hooosofjadsoadfdsf");
         }
     }
-}
+
